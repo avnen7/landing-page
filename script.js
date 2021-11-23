10095f57812895ed37e09d2ef4add59548385725
Avnen <avnen7@gmail.com>
2021-11-23 16:13:55 +0200

    add an alert function to each button

---
+++ script.js
@@ -0,0 +1,11 @@
+// Alert on button clicks
+
+function showAlertOne() {
+  const myText = "😊 THANK YOU FOR CLICKING THIS BUTTON! 😊";
+  alert(myText);
+};
+
+function showAlertTwo() {
+  const myText = "🥰 THANK YOU FOR TRYING THIS ONE TOO! 🥰";
+  alert(myText);
+};
